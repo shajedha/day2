c49a4a53fe416c744e37bfca86e2eaa40548a39f
Shajedha <shajedhapssv.98@gmail.com>
2022-04-21 05:30:48 +0100

    Added:welcome message

--- src/welcomemessage.java
+++ src/welcomemessage.java
@@ -1,5 +1,5 @@
 public class welcomemessage {
     public static void main(String[] args) {
-        System.out.println("welcome to employee wage computation program on master branch");
+        System.out.println("welcome to employee wage");
     }
 }
